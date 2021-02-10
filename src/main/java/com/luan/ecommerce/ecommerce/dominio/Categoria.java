e9faec0135ccb5449261d0c89ffb1d20f8712187
Luan <luanalencar134@gmail.com>
2021-02-09 23:35:34 -0300

    Adicionando anotacoes em Categoria

--- src/main/java/com/luan/ecommerce/ecommerce/dominio/Categoria.java
+++ src/main/java/com/luan/ecommerce/ecommerce/dominio/Categoria.java
@@ -3,13 +3,21 @@ package com.luan.ecommerce.ecommerce.dominio;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "categoria")
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     private Integer id;
+    @Column(name = "descricao")
     private String descricao;
 }
---
+++ src/main/java/com/luan/ecommerce/ecommerce/dominio/Estado.java
@@ -0,0 +1,15 @@
+package com.luan.ecommerce.ecommerce.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+public class Estado implements Serializable {
+
+
+}
