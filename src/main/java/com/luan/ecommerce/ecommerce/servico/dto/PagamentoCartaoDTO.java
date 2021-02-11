daa5f01bb630cfa0ac6ae671ee2262202cae8fdf
Luan <luanalencar134@gmail.com>
2021-02-11 10:27:41 -0300

    adicionando heranca

--- src/main/java/com/luan/ecommerce/ecommerce/servico/dto/PagamentoCartaoDTO.java
+++ src/main/java/com/luan/ecommerce/ecommerce/servico/dto/PagamentoCartaoDTO.java
@@ -1,5 +1,6 @@
 package com.luan.ecommerce.ecommerce.servico.dto;
 
+import com.luan.ecommerce.ecommerce.dominio.Pagamento;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,7 @@ import java.io.Serializable;
 
 @Getter
 @Setter
-public class PagamentoCartaoDTO implements Serializable {
+public class PagamentoCartaoDTO extends Pagamento implements Serializable {
 
     private Integer pedido;
     private Integer parcelas;
