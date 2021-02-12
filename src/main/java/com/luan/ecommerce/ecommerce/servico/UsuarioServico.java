3a1f35761f241c29acc02f4564fd77d037bec526
Luan <luanalencar134@gmail.com>
2021-02-12 17:06:02 -0300

    UsuarioServico desenvolvido

--- src/main/java/com/luan/ecommerce/ecommerce/servico/UsuarioServico.java
+++ src/main/java/com/luan/ecommerce/ecommerce/servico/UsuarioServico.java
@@ -7,6 +7,7 @@ import com.luan.ecommerce.ecommerce.servico.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -25,19 +26,85 @@ public class UsuarioServico {
                 .orElseThrow(() -> new RuntimeException("Usuário não existe!")));
     }
 
-    public UsuarioDTO buscarPorCpf(Usuario usuario) {
-        return usuarioMapper.toDto(usuarioRepositorio.findByCpf(usuario.getCpf()));
+    public UsuarioDTO buscarPorCpf(String cpf) {
+        return usuarioMapper.toDto(usuarioRepositorio.findByCpf(cpf));
     }
 
-    public UsuarioDTO buscarPorEmail(Usuario usuario) {
-        return usuarioMapper.toDto(usuarioRepositorio.findByEmail(usuario.getEmail()));
+    public UsuarioDTO buscarPorEmail(String email) {
+        return usuarioMapper.toDto(usuarioRepositorio.findByEmail(email));
     }
 
-    public UsuarioDTO buscarPorRg(Usuario usuario) {
-        return usuarioMapper.toDto(usuarioRepositorio.findByRg(usuario.getRg()));
+    public UsuarioDTO buscarPorRg(String rg) {
+        return usuarioMapper.toDto(usuarioRepositorio.findByRg(rg));
     }
 
-    public Usuario salvar(Usuario usuario) {
+    public UsuarioDTO salvar(UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioMapper.toEntity(usuarioDTO);
+        validarDadosNull(usuarioDTO);
+        validarIdade(usuarioDTO);
+        validarCpf(usuarioDTO);
+        validarEmail(usuarioDTO);
+        return usuarioMapper.toDto(usuarioRepositorio.save(usuario));
+    }
 
+    public UsuarioDTO editar(UsuarioDTO usuarioDTO) throws RuntimeException {
+        validarDadosNull(usuarioDTO);
+        validarEmail(usuarioDTO);
+        validarCpf(usuarioDTO);
+        validarIdade(usuarioDTO);
+        Usuario usuarioBD = obter(usuarioDTO.getId());
+        Usuario usuario = usuarioMapper.toEntity(usuarioDTO);
+        usuarioRepositorio.save(usuario);
+        return usuarioMapper.toDto(usuario);
+    }
+
+    public void remover(Integer id) {
+        usuarioRepositorio.delete(usuarioRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Id informado não encontrado")));
+    }
+
+    private void validarDadosNull(UsuarioDTO usuario) {
+        if (usuario.getNome() == null) {
+            throw new RuntimeException("Nome de usuário não informado");
+        }
+
+        if (usuario.getCpf() == null) {
+            throw new RuntimeException("Cpf não informado");
+        }
+
+        if (usuario.getEmail() == null) {
+            throw new RuntimeException("E-mail não informado");
+        }
+
+        if (usuario.getDataNascimento() == null) {
+            throw new RuntimeException("Data de nascimento não informada");
+        }
+    }
+
+    private void validarIdade(UsuarioDTO usuario) {
+        int idade = LocalDate.now().getYear() - usuario.getDataNascimento().getYear();
+        if (idade > 115 || idade < 10) {
+            throw new RuntimeException("Data de nascimento inválida");
+        }
+    }
+
+
+    private Usuario obter(Integer id) {
+        return usuarioRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    private void validarEmail(UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioRepositorio.findByEmail(usuarioDTO.getEmail());
+        if (usuario != null && !usuario.getId().equals(usuarioDTO.getId())) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+    }
+
+    private void validarCpf(UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioRepositorio.findByCpf(usuarioDTO.getCpf());
+        if (usuario != null && !usuario.getId().equals(usuarioDTO.getId())) {
+            throw new RuntimeException("CPF já cadastrado");
+        }
     }
 }
