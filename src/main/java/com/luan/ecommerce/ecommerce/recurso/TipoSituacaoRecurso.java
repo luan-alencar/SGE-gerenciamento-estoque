3d473a748db2b6da6492a45556dc2b7eafe2c420
Luan <luanalencar134@gmail.com>
2021-02-16 16:58:28 -0300

    Adicionando Recurso, Servico e Mapper - TipoSituacao

--- src/main/java/com/luan/ecommerce/ecommerce/recurso/TipoSituacaoRecurso.java
+++ src/main/java/com/luan/ecommerce/ecommerce/recurso/TipoSituacaoRecurso.java
@@ -1,2 +1,32 @@
-package com.luan.ecommerce.ecommerce.recurso;public class TipoSituacaoRecurso {
+package com.luan.ecommerce.ecommerce.recurso;
+
+import com.luan.ecommerce.ecommerce.dominio.TipoSituacao;
+import com.luan.ecommerce.ecommerce.servico.TipoSituacaoServico;
+import com.luan.ecommerce.ecommerce.servico.dto.TipoSituacaoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "api/tipossituacao")
+public class TipoSituacaoRecurso {
+
+    private final TipoSituacaoServico tipoSituacaoServico;
+
+    @GetMapping
+    public ResponseEntity<List<TipoSituacaoDTO>> listar() {
+        return ResponseEntity.ok(tipoSituacaoServico.listar());
+    }
+
+    @GetMapping("/{idTipoSituacao}")
+    public ResponseEntity<TipoSituacaoDTO> buscarPorId(@PathVariable Integer idTipoSituacao) {
+        return ResponseEntity.ok(tipoSituacaoServico.buscarPorId(idTipoSituacao));
+    }
 }
--- src/main/java/com/luan/ecommerce/ecommerce/servico/mapper/TipoSituacaoMapper.java
+++ src/main/java/com/luan/ecommerce/ecommerce/servico/mapper/TipoSituacaoMapper.java
@@ -1,2 +1,9 @@
-package com.luan.ecommerce.ecommerce.servico.mapper;public interface TipoSituacaoMapper {
+package com.luan.ecommerce.ecommerce.servico.mapper;
+
+import com.luan.ecommerce.ecommerce.dominio.TipoSituacao;
+import com.luan.ecommerce.ecommerce.servico.dto.TipoSituacaoDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface TipoSituacaoMapper extends EntityMapper<TipoSituacaoDTO, TipoSituacao> {
 }
