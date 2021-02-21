0d5a9358d412de354296bb5c0e08bf478d8aba88
Luan <luanalencar134@gmail.com>
2021-02-21 20:56:18 -0300

    Alterancoes - Adicionando mensagens de cadastro success

--- frontend/src/app/modulos/produto/components/produto-listagem/produto-listagem.component.ts
+++ frontend/src/app/modulos/produto/components/produto-listagem/produto-listagem.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { ConfirmationService, MenuItem } from 'primeng/api';
+import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
 import { Produto } from 'src/app/dominio/produto';
 import { ProdutoService } from '../../services/produto.service';
 
@@ -23,13 +23,27 @@ export class ProdutoListagemComponent implements OnInit {
   constructor(
     private produtoService: ProdutoService,
     private confirmationService: ConfirmationService,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService
   ) { }
 
   ngOnInit(): void {
 
     this.buscarProdutos();
 
+  }
+
+  addSingle() {
+    this.messageService.add({ severity: 'success', summary: 'Delete de Produto', detail: 'Produto apagado com sucesso!' });
+  }
+
+  addMultiple() {
+    this.messageService.addAll([{ severity: 'success', summary: 'Cadastro de Produto', detail: 'Cadastro realizado' },
+    { severity: 'info', summary: 'Editado!', detail: 'O produto foi editado com sucesso' }]);
+  }
+
+  clear() {
+    this.messageService.clear();
   }
 
   showDialog(id: number) {
@@ -50,6 +64,7 @@ export class ProdutoListagemComponent implements OnInit {
       message: 'Deseja confirmar excluir o produto?',
       accept: () => {
         this.deletarProduto(id);
+        this.addSingle();
       }
     })
   }
@@ -64,10 +79,9 @@ export class ProdutoListagemComponent implements OnInit {
   deletarProduto(id: number) {
     this.produtoService.deletarProduto(id)
       .subscribe(() => {
-        alert('Produto deletado!');
         setTimeout(() => {
           this.router.navigate(['/produtos'])
-        }, 1500);
+        }, 1800);
         this.buscarProdutos();
       }, err => {
         alert('Dados inválidos!');
