89cc6f98ca3a50bc731f5b5c775af0491c975fa4
Luan <luanalencar134@gmail.com>
2021-03-01 12:41:07 -0300

    Removendo component editar-produto e criando dialog para cadastrar produto

--- frontend/src/app/modules/produto/produto.module.ts
+++ frontend/src/app/modules/produto/produto.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { InputNumberModule } from 'primeng';
 import { CardModule } from 'primeng/card';
 import { SharedModule } from '../../shared/shared.module';
-import { EditarProdutoComponent } from './components/editar-produto/editar-produto.component';
 import { InformacaoProdutoComponent } from './components/informacao-produto/informacao-produto.component';
 import { ProdutoCadastroComponent } from './components/produto-cadastro/produto-cadastro.component';
 import { ProdutoListagemComponent } from './components/produto-listagem/produto-listagem.component';
@@ -15,7 +14,7 @@ import { ProdutoService } from './services/produto.service';
 
 
 @NgModule({
-  declarations: [ProdutoListagemComponent, ProdutoCadastroComponent, InformacaoProdutoComponent, EditarProdutoComponent],
+  declarations: [ProdutoListagemComponent, ProdutoCadastroComponent, InformacaoProdutoComponent],
   providers: [
     ProdutoService
   ],
