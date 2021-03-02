a60abb7d2f13e0c45a19c3e5e774bd7b51f23fa9
Luan <luanalencar134@gmail.com>
2021-03-02 20:44:25 -0300

    Comecando a parte de Usuario

--- frontend/src/app/modules/usuario/usuario-routing.module.ts
+++ frontend/src/app/modules/usuario/usuario-routing.module.ts
@@ -1,8 +1,14 @@
+import { UsuarioFormularioComponent } from './usuario-formulario/usuario-formulario.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsuarioFormularioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/modules/usuario/usuario.module.ts
+++ frontend/src/app/modules/usuario/usuario.module.ts
@@ -1,15 +1,25 @@
+import { InputMaskModule } from 'primeng/inputmask';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { UsuarioFormularioComponent } from './usuario-formulario/usuario-formulario.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardModule, InputMask, InputNumberModule, SharedModule } from 'primeng';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [UsuarioFormularioComponent],
   imports: [
     CommonModule,
-    UsuarioRoutingModule
+    UsuarioRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+    HttpClientModule,
+    CardModule,
+    InputNumberModule,
+    InputMaskModule
   ]
 })
 export class UsuarioModule { }
