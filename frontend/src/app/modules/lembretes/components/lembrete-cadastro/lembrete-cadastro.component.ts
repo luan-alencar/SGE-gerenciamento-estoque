5ba6c19f8a2c8242dddda241b5208109881c054b
Luan <luanalencar134@gmail.com>
2021-03-05 21:45:30 -0300

    commits

---
+++ frontend/src/app/modules/lembretes/components/lembrete-cadastro/lembrete-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lembrete-cadastro',
+  templateUrl: './lembrete-cadastro.component.html',
+  styleUrls: ['./lembrete-cadastro.component.css']
+})
+export class LembreteCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
