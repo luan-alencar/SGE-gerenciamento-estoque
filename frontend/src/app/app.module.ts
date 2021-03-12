66a7e165c73f741344bccf0d6bf4fcff67df0f81
luan-alencar <luanalencar134@gmail.com>
2021-03-12 17:15:49 -0300

    adicionando component combo chart (ainda implementando)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SecurityModule, VersionTagModule } from '@nuvem/angular-base';
 import { BreadcrumbModule, ErrorStackModule, MenuModule, PageNotificationModule } from '@nuvem/primeng-components';
 import { BlockUIModule } from 'ng-block-ui';
-import { ButtonModule, CalendarModule, ConfirmDialogModule, ContextMenuModule, DialogModule, DropdownModule, FileUploadModule, InputNumberModule, InputTextareaModule, InputTextModule, MultiSelectModule, ProgressBarModule, RadioButtonModule, RatingModule, SliderModule, TableModule, ToastModule, ToolbarModule } from 'primeng';
+import { ButtonModule, CalendarModule, ChartModule, ConfirmDialogModule, ContextMenuModule, DialogModule, DropdownModule, FileUploadModule, InputNumberModule, InputTextareaModule, InputTextModule, MultiSelectModule, ProgressBarModule, RadioButtonModule, RatingModule, SliderModule, TableModule, TabViewModule, ToastModule, ToolbarModule } from 'primeng';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,12 +18,14 @@ import { EventService } from './modules/lembrete/services/event.service';
 import { ProdutoService } from './modules/produto/services/produto.service';
 import { UsuarioService } from './modules/usuario/services/usuario.service';
 import { UsuarioListagemComponent } from './modules/usuario/usuario-listagem/usuario-listagem.component';
+import { AppConfigService } from './services/appconfigservice';
 import { LoginComponent } from './shared/components/login/login.component';
 import { SharedModule } from './shared/shared.module';
 
 
 
 @NgModule({
+    
     declarations: [
         AppComponent,
         AppTopbarComponent,
@@ -32,6 +34,7 @@ import { SharedModule } from './shared/shared.module';
         HomeComponent,
         UsuarioListagemComponent
     ],
+
     imports: [
         BlockUIModule.forRoot({
             message: "Carregando..."
@@ -67,12 +70,27 @@ import { SharedModule } from './shared/shared.module';
         RadioButtonModule,
         InputNumberModule,
         ConfirmDialogModule,
-        InputTextareaModule
+        InputTextareaModule,
+        CommonModule,
+        ChartModule,
+        ToastModule,
+        TabViewModule
     ],
-    providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy }, ProdutoService, UsuarioService, LoginComponent, EventService
 
+    providers: [
+        {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
+        },
+
+        ProdutoService,
+        UsuarioService,
+        LoginComponent,
+        EventService,
+        AppConfigService
     ],
+
     bootstrap: [AppComponent]
 })
+
 export class AppModule { }
